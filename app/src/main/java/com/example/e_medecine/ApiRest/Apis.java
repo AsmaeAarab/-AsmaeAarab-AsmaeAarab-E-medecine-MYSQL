14f6412135fad500184934134b91c0638bc7a54b
meriem <meriem.dch8@gmail.com>
2021-02-15 19:54:25 +0100

    apis

--- app/src/main/java/com/example/e_medecine/ApiRest/Apis.java
+++ app/src/main/java/com/example/e_medecine/ApiRest/Apis.java
@@ -4,10 +4,10 @@ package com.example.e_medecine.ApiRest;
 public class Apis {
     //public static final String URL_002="http://192.168.56.1:8080/patients/";
    // public static final String URL_001="http://192.168.56.1:8080/user/";
-    public static final String URL_002="http://192.168.1.5:8080/patients/";
-    public static final String URL_001="http://192.168.1.5:8080/user/";
-    public static final String URL_003="http://192.168.1.108:8080/specialites/";
-    public static final String URL_004="http://192.168.1.108:8080/rdv/";
+    public static final String URL_002="http://192.168.1.9:8080/patients/";
+    public static final String URL_001="http://192.168.1.9:8080/user/";
+    public static final String URL_003="http://192.168.1.9:8080/specialites/";
+    public static final String URL_004="http://192.168.1.9:8080/rdv/";
     public static PatientService getPatientsService(){
         return  Cliente.getCliente(URL_002).create(PatientService.class);
     }
